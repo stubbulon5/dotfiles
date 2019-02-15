562ab1ccbb7a9c24b62969cf672cda0911a992be
Stu <42404319+stubbulon5@users.noreply.github.com>
2019-02-15 21:28:03 +0000

    Update ubuntu-spectre-x360.sh

--- ubuntu-spectre-x360.sh
+++ ubuntu-spectre-x360.sh
@@ -4,3 +4,11 @@
 # https://ubuntuforums.org/showthread.php?t=2391805
 sudo apt install xserver-xorg-input-synaptics
 
+# Graphics
+# https://askubuntu.com/questions/1065852/how-to-upgrade-intel-graphics-driver
+sudo add-apt-repository ppa:oibaf/graphics-drivers
+sudo apt dist-upgrade
+
+
+# To remove
+ppa-purge ppa:oibaf/graphics-drivers
